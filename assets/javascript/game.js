365f42cdfbef1056b23e7d0761911e24febf0737
Patrick Hernandez <r.patrick.hernandez@hotmail.com>
2016-04-29 02:34:35 -0400

    Added title

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+
+	var r1s1 = $("#r1s1");
+	var r1s6 = $("#r1s6");
+	var red = $("#redbox");
+
+	r1s1.on("click", function(){
+		r1s6.append(red);
+	});
+
+
+});
+
--- game.js
+++ game.js
@@ -1,4 +1,15 @@
 
-$(".top").on("click", function(){
+$(document).ready(function() {
+	console.log('i work')
+
+	var r1s1 = $("#r1s1");
+	var r1s6 = $("#r1s6");
+	var red = $("#redbox");
+
+	r1s1.on("click", function(){
+		r1s6.html(red);
+	});
+
+
 });
 
